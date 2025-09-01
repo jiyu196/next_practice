60182ab3ad38d8602b7ad7af38d40d5ebdc39a68
jiyu196 <jiyu16413@gmail.com>
2025-09-01 23:46:26 +0900

    next 오늘 배운부분 설명 추가함

--- src/app/board/page.js
+++ src/app/board/page.js
@@ -11,3 +11,7 @@ export default function Home() {
     </div>
   );
 }
+
+// ---------------------------경로 밑에 경로--------------------------------
+// localhost:3030/board 밑에  /rental 경로도 붙일 수 있는지 실습해봄
+// app 경로 밑에 board 경로 만들었던 것처럼 board밑에 rental경로를 생성하니 가능함
